e64628d477eed9c7dacbcbe33c19a211cbc9ebac
playdingnow <playdingnow@gmail.gf.com.cn>
2022-09-13 14:39:02 +0800

    test: more test about lru mem cache

--- test/cache.test.js
+++ test/cache.test.js
@@ -17,7 +17,8 @@ const opt = {
 const client = new Redis(opt.redis);
 const cacher = new Cacher(opt);
 
-const KEY = `${opt.prefix}getShopes`;
+const KEY_INPUT = `getShopes`;
+const KEY = opt.prefix + KEY_INPUT;
 const getShopes = async (type) => {
   if (type === 0) {
     throw new Error('bad params');
@@ -34,15 +35,125 @@ const getShopes = async (type) => {
   return ['shop01', 'shop02'];
 };
 
-test.beforeEach(async () => client.flushdb());
+test.beforeEach(async () => {
+  await client.flushdb();
+  cacher.mem.clear();
+});
 
 const wait = async (time = 10) => {
   await new Promise((resolve) => setTimeout(resolve, time));
 };
 
+test('cache: should read mem cache', async (t) => {
+  await client.set(KEY, '"ding"', 'px', cacher.minRedisTtl * 2);
+
+  t.is(cacher.mem.get(KEY), undefined);
+  await cacher.get({
+    key: KEY_INPUT,
+    executor: getShopes.bind(null, 3),
+    expire: 10,
+    mem: true,
+  });
+  await wait();
+  t.is(cacher.mem.get(KEY), 'ding');
+
+  await client.set(KEY, '"dingding"', 'px', cacher.minRedisTtl * 2);
+  await cacher.get({
+    key: KEY_INPUT,
+    executor: getShopes.bind(null, 3),
+    expire: 10,
+    mem: true,
+  });
+  await wait();
+  t.is(cacher.mem.get(KEY), 'ding');
+});
+
+test('cache: should not cache redis data to mem when redis ttl to short', async (t) => {
+  await client.set(KEY, '"ding"', 'px', cacher.minRedisTtl - 1);
+
+  t.is(cacher.mem.get(KEY), undefined);
+  await cacher.get({
+    key: KEY_INPUT,
+    executor: getShopes.bind(null, 3),
+    expire: 10,
+    mem: true,
+  });
+  await wait();
+  t.is(cacher.mem.get(KEY), undefined);
+});
+
+test('cache: should not cache redis data to mem in first try', async (t) => {
+  t.is(cacher.mem.get(KEY), undefined);
+  await cacher.get({
+    key: KEY_INPUT,
+    executor: _.noop,
+    expire: 10,
+    mem: true,
+  });
+  await wait();
+  t.is(cacher.mem.get(KEY), undefined);
+});
+
+test('cache: should cache redis data to mem in second try', async (t) => {
+  let count = 0;
+  t.is(cacher.mem.get(KEY), undefined);
+  await cacher.get({
+    key: KEY_INPUT,
+    executor: () => {
+      count++;
+      return 'ding';
+    },
+    expire: 10,
+    mem: true,
+  });
+  await wait();
+  t.is(cacher.mem.get(KEY), undefined);
+  t.is(count, 1);
+
+  await cacher.get({
+    key: KEY_INPUT,
+    executor: () => {
+      count++;
+      return 'ding';
+    },
+    expire: 10,
+    mem: true,
+  });
+  await wait();
+  t.is(count, 1);
+  t.is(cacher.mem.get(KEY), 'ding');
+  // 第二次 get 的时候已经是 10ms 之后了
+  // 而放到内存是也应该小于 epxire 的 10s
+  t.true(cacher.mem.getRemainingTTL(KEY) < 10000);
+});
+
+test('cache: should cache redis data to mem', async (t) => {
+  await client.set(KEY, '"ding"', 'ex', 10);
+
+  t.is(cacher.mem.get(KEY), undefined);
+  await cacher.get({
+    key: KEY_INPUT,
+    executor: _.noop,
+    expire: 10,
+    mem: true,
+  });
+  await wait();
+  t.is(cacher.mem.get(KEY), 'ding');
+});
+
+test('cache: should support mem false', async (t) => {
+  const instance = new Cacher({ mem: false });
+  t.is(instance.mem, undefined);
+});
+
+test('cache: should support mem in default', async (t) => {
+  const instance = new Cacher();
+  t.not(instance.mem, undefined);
+});
+
 test('cache: should support raw:false', async (t) => {
   const payload = {
-    key: 'getShopes',
+    key: KEY_INPUT,
     executor: getShopes.bind(null, 3),
     raw: false,
   };
@@ -55,7 +166,7 @@ test('cache: should support raw:false', async (t) => {
 
 test('cache: should support raw param with data<string>', async (t) => {
   const payload = {
-    key: 'getShopes',
+    key: KEY_INPUT,
     executor: getShopes.bind(null, 3),
     raw: true,
   };
@@ -68,7 +179,7 @@ test('cache: should support raw param with data<string>', async (t) => {
 
 test('cache: should cache results in redis', async (t) => {
   const payload = {
-    key: 'getShopes',
+    key: KEY_INPUT,
     executor: getShopes.bind(null, 1),
   };
   return cacher
@@ -82,7 +193,7 @@ test('cache: should cache results in redis', async (t) => {
 
 test('cache: should not cache when executor reject', async (t) => {
   const payload = {
-    key: 'getShopes',
+    key: KEY_INPUT,
     executor: getShopes.bind(null, 0),
   };
   return cacher
@@ -96,7 +207,7 @@ test('cache: should not cache when executor reject', async (t) => {
 
 test('cache: should return cache data when hit', async (t) => {
   const payload = {
-    key: 'getShopes',
+    key: KEY_INPUT,
     executor: getShopes.bind(null, 1),
     expire: 100,
   };
@@ -111,7 +222,7 @@ test('cache: should return cache data when hit', async (t) => {
 
 test('cache: should recache when cache is outdated', async (t) => {
   const payload = {
-    key: 'getShopes',
+    key: KEY_INPUT,
     executor: getShopes.bind(null, 1),
   };
   await client.set(KEY, '["shop01"]', 'px', 10);
@@ -126,7 +237,7 @@ test('cache: should recache when cache is outdated', async (t) => {
 
 test('cache: should use default expire when expire is less than 0', async (t) => {
   const payload = {
-    key: 'getShopes',
+    key: KEY_INPUT,
     executor: getShopes.bind(null, 1),
     expire: -1,
   };
@@ -142,7 +253,7 @@ test('cache: should use default expire when expire is less than 0', async (t) =>
 
 test('cache: should not cache when return an undefined by executor', async (t) => {
   const payload = {
-    key: 'getShopes',
+    key: KEY_INPUT,
     executor: getShopes.bind(null, 2),
   };
   return cacher
@@ -155,7 +266,7 @@ test('cache: should not cache when return an undefined by executor', async (t) =
 });
 
 test('cache: should remove the key in cache', async (t) => {
-  const key = 'getShopes';
+  const key = KEY_INPUT;
   return client
     .set(KEY, '["shop01"]')
     .then(() => cacher.delete(key))
